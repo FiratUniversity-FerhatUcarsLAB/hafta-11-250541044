b8132fbf6f554f921eddc0791e0c3b30ed95c1ee
250541044 <250541044@firat.edu.tr>
2025-12-03 21:19:30 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,43 @@
+//EREN BİLGE KOÇAK
+//TEKNOLOJİ FAKÜLTESİ
+//YAZILIM MÜHENDİSLİĞİ
+//ÖDEV 1
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
-
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle", 13);               // 1
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp);         // 5
+        zippo("ping", -5);                 // 6
     }
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+        if (flag < 0) {                    // 2, 7
+            System.out.println(quince + " zoop"); // 8
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");      // 3
+            baffle(quince);                // 4
+            System.out.println("boo-wa-ha-ha");   // 9
         }
     }
 }
+
+/*
+GÖREV SORULARININ CEVAPLARI:
+
+1. Çalışma sırası yukarıdaki kod satırlarının yanına (// 1, // 2...) şeklinde işlenmiştir.
+
+2. baffle metodu ilk çağrıldığında blimp parametresinin değeri nedir?
+   Cevap: "rattle"
+
+3. Programın ürettiği tüm çıktı:
+   ik
+   rattle
+   ping zoop
+   boo-wa-ha-ha
+
+4. Hangi satırlar birden fazla kez çalışmıştır?
+   Cevap: "if (flag < 0)" satırı (Sıra 2 ve 7'de olmak üzere iki kez çalışmıştır).
+*/
