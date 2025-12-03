948653b8236b64078785938c5a8879e9ee3f9b3d
250541044 <250541044@firat.edu.tr>
2025-12-03 21:16:00 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,40 @@
-public class Exercise4_1 {
+//EREN BİLGE KOÇAK
+//TEKNOLOJİ FAKÜLTESİ
+//YAZILIM MÜHENDİSLİĞİ
+//ÖDEV 1
+//B SINIFI
 
+
+
+
+public class dsd{
+
+    // Amerikan Formati: Day, Month Date, Year (Ornek: Monday, July 22, 2019)
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println("Amerikan Formati");
+        // String birlestirme (concatenation)
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
+    // Avrupa Formati: Date Month Year, Day (Ornek: 22 July 2019, Monday)
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println("Avrupa Formati");
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        // Degiskenlerin tanimlanmasi
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        // Metotlarin cagirilmasi
+        printAmerican(day, date, month, year);
+
+        // Cikti karismasin diye araya bosluk ekledim
+        System.out.println();
+
+        printEuropean(day, date, month, year);
     }
 }
