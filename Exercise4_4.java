70e29bf29faee86693814b22f4f02c3a93948fac
250541044 <250541044@firat.edu.tr>
2025-12-03 21:33:46 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -12,10 +12,24 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        /* CEVAP 1:
+           Kod sorunsuz bir şekilde derlenir ve çalışır. Hiçbir hata veya uyarı vermez.
+           Metot çalışır, hesaplamasını yapar ancak döndürdüğü değer (42) hiçbir 
+           değişkene atanmadığı için anında kaybolur (discarded). Java'da dönüş 
+           değerini görmezden gelmek yasaldır.
+        */
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        /* CEVAP 2:
+           Bu satırın yorumunu kaldırırsanız "Derleme Hatası" (Compile-time Error) alırsınız.
+           
+           Hata Mesajı: "Operator '+' cannot be applied to 'void' and 'int'" 
+           (veya benzeri: "void type not allowed here")
+           
+           Açıklama: void metotlar geriye bir veri (sayı, string vb.) döndürmez. 
+           Olmayan bir değerle (hiçlik ile) 7 sayısını toplayamazsınız. 
+           Bu yüzden Java bu ifadeyi derleyemez.
+        */
     }
 }
